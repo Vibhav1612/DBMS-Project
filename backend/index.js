49975d8d03c027e6f411ff30c126be891c34f161
Vibhav1612 <vibhav.vasudevan@gmail.com>
2023-11-11 16:53:34 +0530

    working till insertion into club_user

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'vibhav123',
-  database: 'club_management'
+  database: 'club_management',
+  Promise: global.Promise
+
 });
 
 db.connect((err) => {
@@ -118,39 +120,89 @@ app.get('/club_events_count', (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const username = req.body.username; // Adjusted to match the frontend
+  const username = req.body.username;
   const password = req.body.password;
 
-  db.query("INSERT INTO users(email,password) VALUES(?,?)", [username, password], (err, result) => {
+  // Perform INSERT
+  db.query("INSERT INTO users(email, password) VALUES(?, ?)", [username, password], (err, result) => {
     if (err) {
-      console.log(err);
+      console.error(err);
       res.status(500).json({ message: "Error registering user" });
     } else {
-      res.status(200).json({ message: "User registered successfully" });
+      // Perform SELECT to get the updated user_id
+      db.query("SELECT user_id FROM users WHERE email = ?", [username], (selectErr, selectResult) => {
+        if (selectErr) {
+          console.error(selectErr);
+          res.status(500).json({ message: "Error retrieving user_id" });
+        } else {
+          const userId = selectResult[0] ? selectResult[0].user_id : null; // Check if selectResult[0] is defined
+          console.log("User registered successfully. UserID:", userId);
+          res.status(200).json({ userId, message: "User registered successfully" });
+        }
+      });
     }
   });
 });
 
-app.post("/register_clubs/:userId", (req, res) => {
+
+
+import util from 'util';  // Import the util module
+
+// ...
+
+const queryAsync = util.promisify(db.query).bind(db);  // Promisify the query method
+
+app.post("/:userId/register_clubs", async (req, res) => {
   const userId = req.params.userId;
   const { clubs } = req.body;
+  console.log(clubs)
 
-  // Assuming 'clubs' is an array of club names
-  // Iterate through the clubs and insert into Club_User table
-  clubs.forEach((club) => {
-    db.query(
-      "INSERT INTO Club_User (UserID, ClubName) VALUES (?, ?)",
-      [userId, club],
-      (err, result) => {
-        if (err) { 
-          console.log(err);
-        }
+  try {
+    const clubIds = [];
+
+    for (const clubName of clubs) {
+      // Use the promisified query method
+      const [rows] = await queryAsync('SELECT clubid FROM club WHERE ClubName = ?', [clubName]);
+      console.log(typeof rows)
+
+      if (!rows || typeof rows !== 'object' || !('clubid' in rows) || typeof rows.clubid === 'undefined') {
+        console.error(`Invalid data received from the database for club '${clubName}'`);
+        continue; // Skip to the next iteration if data is invalid
       }
-    );
-  });
+      
+      
+      console.log('Illi')
+      const clubData = rows;
+      console.log('ClubData', clubData)
+      clubIds.push(clubData.clubid);
+      console.log(clubIds)
+    }
 
-  res.json({ message: 'Clubs registered successfully' });
+    // Ensure that clubIds is an array before trying to filter and map
+    if (!Array.isArray(clubIds)) {
+      throw new Error("Invalid data received from the database");
+    }
+
+    const validClubIds = clubIds.filter((clubId) => clubId !== null);
+
+    // Ensure that validClubIds is an array before trying to map
+    if (!Array.isArray(validClubIds)) {
+      throw new Error("Invalid data received from the database");
+    }
+
+    await Promise.all(validClubIds.map(async (clubId) => {
+      await db.query('INSERT INTO Club_User (User_ID, ClubID) VALUES (?, ?)', [userId, clubId]);
+    }));
+
+    res.json({ message: 'Clubs registered successfully' });
+  } catch (error) {
+    console.error("Error during club registration here:", error);
+    res.status(500).json({ message: "Error during club registration" });
+  }
 });
+
+
+
 
 
 
