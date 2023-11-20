ca078641f1bb1af9f5df904d3f2206a2aa24fe32
Vibhav1612 <vibhav.vasudevan@gmail.com>
2023-11-20 11:52:54 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,133 @@ app.get('/', (req, res) => {
   res.json('This is the backend server');
 });
 
+// Register
+
+//////////////////////////////////////////////////////////
+
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Perform INSERT
+  db.query("INSERT INTO users(email, password) VALUES(?, ?)", [username, password], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: "Error registering user" });
+    } else {
+      // Perform SELECT to get the updated user_id
+      db.query("SELECT user_id FROM users WHERE email = ?", [username], (selectErr, selectResult) => {
+        if (selectErr) {
+          console.error(selectErr);
+          res.status(500).json({ message: "Error retrieving user_id" });
+        } else {
+          const userId = selectResult[0] ? selectResult[0].user_id : null; 
+          console.log("User registered successfully. UserID:", userId);
+          res.status(200).json({ userId, message: "User registered successfully" });
+        }
+      });
+    }
+  });
+});
+
+/////////////////////////////////////////////////////////////
+
+
+// Registering Clubs for users
+
+//////////////////////////////////////////////////////////////
+
+import util from 'util';  
+
+
+const queryAsync = util.promisify(db.query).bind(db); 
+
+app.post("/:userId/register_clubs", async (req, res) => {
+  const userId = req.params.userId;
+  const { clubs } = req.body;
+  console.log(clubs)
+
+  try {
+    const clubIds = [];
+
+    for (const clubName of clubs) {
+      // Use the promisified query method
+      const [rows] = await queryAsync('SELECT clubid FROM club WHERE ClubName = ?', [clubName]);
+      console.log(typeof rows)
+
+      if (!rows || typeof rows !== 'object' || !('clubid' in rows) || typeof rows.clubid === 'undefined') {
+        console.error(`Invalid data received from the database for club '${clubName}'`);
+        continue; // Skip to the next iteration if data is invalid
+      }
+      
+      
+      const clubData = rows;
+      console.log('ClubData', clubData)
+      clubIds.push(clubData.clubid);
+      console.log(clubIds)
+    }
+
+    if (!Array.isArray(clubIds)) {
+      throw new Error("Invalid data received from the database");
+    }
+
+    const validClubIds = clubIds.filter((clubId) => clubId !== null);
+
+    if (!Array.isArray(validClubIds)) {
+      throw new Error("Invalid data received from the database");
+    }
+
+    await Promise.all(validClubIds.map(async (clubId) => {
+      await db.query('INSERT INTO Club_User (User_ID, ClubID) VALUES (?, ?)', [userId, clubId]);
+    }));
+
+    res.json({ message: 'Clubs registered successfully' });
+  } catch (error) {
+    console.error("Error during club registration here:", error);
+    res.status(500).json({ message: "Error during club registration" });
+  }
+});
+
+
+
+///////////////////////////////////////////////////////////////
+
+//Login
+
+///////////////////////////////////////////////////
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT user_id FROM users WHERE email = ? AND password = ?",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ message: "Error during login", error: err });
+      } else {
+        if (result.length > 0) {
+          console.log(result)
+          const userId = result[0].user_id;
+          console.log(userId)
+          res.status(200).json({ userId, message: "Login successful" });
+        } else {
+          res.status(401).json({ message: "Wrong username/password!" });
+        }
+      }
+    }
+  );
+});
+
+//////////////////////////////////////////////////////////
+
+
+// Clubs with category filtering
+
+///////////////////////////////////////////////////////
+
+
 app.get('/club', (req, res) => {
   const { category } = req.query;
 
@@ -60,6 +187,14 @@ app.get('/club', (req, res) => {
   }
 });
 
+
+//////////////////////////////////////////////////////////
+
+// Events
+
+
+/////////////////////////////////////////////////////////////
+
 app.get('/events', (req, res) => {
   const query = 'SELECT * FROM EVENT WHERE DATE_TIME > CURTIME()';
   db.query(query, (err, data) => {
@@ -70,6 +205,13 @@ app.get('/events', (req, res) => {
   });
 });
 
+
+/////////////////////////////////////////////////////////
+
+// Announcements
+
+//////////////////////////////////////////////////////////
+
 app.get('/announcements', (req, res) => {
   const query = 'SELECT * FROM ANNOUNCEMENT';
   db.query(query, (err, data) => {
@@ -79,6 +221,13 @@ app.get('/announcements', (req, res) => {
     return res.json(data);
   });
 });
+
+///////////////////////////////////////////////////////////
+
+
+// Recruitments
+
+///////////////////////////////////////////////////////////
 
 app.get('/recruitments', (req, res) => {
   const searchTerm = 'recruit'; 
@@ -91,6 +240,14 @@ app.get('/recruitments', (req, res) => {
   });
 });
 
+/////////////////////////////////////////////////////
+
+
+
+// Seminars
+
+//////////////////////////////////////////////////////
+
 
 app.get('/seminars', (req, res) => {
   const searchTerm = 'seminar'; 
@@ -102,6 +259,14 @@ app.get('/seminars', (req, res) => {
     return res.json(data[0]); 
   });
 });
+
+
+//////////////////////////////////////////////////////
+
+
+// Retrieving the number of events hosted by each club
+
+///////////////////////////////////////////////////////
 
 app.get('/club_events_count', (req, res) => {
   const query = `
@@ -119,117 +284,14 @@ app.get('/club_events_count', (req, res) => {
   });
 });
 
-app.post("/register", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  // Perform INSERT
-  db.query("INSERT INTO users(email, password) VALUES(?, ?)", [username, password], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: "Error registering user" });
-    } else {
-      // Perform SELECT to get the updated user_id
-      db.query("SELECT user_id FROM users WHERE email = ?", [username], (selectErr, selectResult) => {
-        if (selectErr) {
-          console.error(selectErr);
-          res.status(500).json({ message: "Error retrieving user_id" });
-        } else {
-          const userId = selectResult[0] ? selectResult[0].user_id : null; // Check if selectResult[0] is defined
-          console.log("User registered successfully. UserID:", userId);
-          res.status(200).json({ userId, message: "User registered successfully" });
-        }
-      });
-    }
-  });
-});
 
 
-
-import util from 'util';  // Import the util module
-
-// ...
-
-const queryAsync = util.promisify(db.query).bind(db);  // Promisify the query method
-
-app.post("/:userId/register_clubs", async (req, res) => {
-  const userId = req.params.userId;
-  const { clubs } = req.body;
-  console.log(clubs)
-
-  try {
-    const clubIds = [];
-
-    for (const clubName of clubs) {
-      // Use the promisified query method
-      const [rows] = await queryAsync('SELECT clubid FROM club WHERE ClubName = ?', [clubName]);
-      console.log(typeof rows)
-
-      if (!rows || typeof rows !== 'object' || !('clubid' in rows) || typeof rows.clubid === 'undefined') {
-        console.error(`Invalid data received from the database for club '${clubName}'`);
-        continue; // Skip to the next iteration if data is invalid
-      }
-      
-      
-      console.log('Illi')
-      const clubData = rows;
-      console.log('ClubData', clubData)
-      clubIds.push(clubData.clubid);
-      console.log(clubIds)
-    }
-
-    // Ensure that clubIds is an array before trying to filter and map
-    if (!Array.isArray(clubIds)) {
-      throw new Error("Invalid data received from the database");
-    }
-
-    const validClubIds = clubIds.filter((clubId) => clubId !== null);
-
-    // Ensure that validClubIds is an array before trying to map
-    if (!Array.isArray(validClubIds)) {
-      throw new Error("Invalid data received from the database");
-    }
-
-    await Promise.all(validClubIds.map(async (clubId) => {
-      await db.query('INSERT INTO Club_User (User_ID, ClubID) VALUES (?, ?)', [userId, clubId]);
-    }));
-
-    res.json({ message: 'Clubs registered successfully' });
-  } catch (error) {
-    console.error("Error during club registration here:", error);
-    res.status(500).json({ message: "Error during club registration" });
-  }
-});
+///////////////////////////////////////////////////////
 
 
+// Retrieving club_specific meetings for users
 
-
-
-
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  db.query(
-    "SELECT user_id FROM users WHERE email = ? AND password = ?",
-    [username, password],
-    (err, result) => {
-      if (err) {
-        res.status(500).json({ message: "Error during login", error: err });
-      } else {
-        if (result.length > 0) {
-          console.log(result)
-          const userId = result[0].user_id;
-          console.log(userId)
-          res.status(200).json({ userId, message: "Login successful" });
-        } else {
-          res.status(401).json({ message: "Wrong username/password!" });
-        }
-      }
-    }
-  );
-});
-
+////////////////////////////////////////////////////////
 
 app.get('/:userId/meetings', async (req, res) => {
   const userId = req.params.userId;
@@ -243,15 +305,18 @@ app.get('/:userId/meetings', async (req, res) => {
 });
 
 
+//////////////////////////////////////////////////////////
 
+
+// Creation of meeting for users
+
+///////////////////////////////////////////////////////////
 
 app.post('/:userId/meetings', async (req, res) => {
   const userId = req.params.userId;
   const { agenda, date_time } = req.body;
 
   try {
-    // Fetch the first club ID associated with the user
-    console.log('here')
     const clubQuery = 'SELECT ClubID FROM Club_User WHERE User_ID = ? LIMIT 1';
     const [clubResult] = await queryAsync(clubQuery, [userId]);
     console.log(clubResult)
@@ -273,7 +338,11 @@ app.post('/:userId/meetings', async (req, res) => {
   }
 });
 
+/////////////////////////////////////////////////////////
 
+// Updating meetings
+
+/////////////////////////////////////////////////////////
 
 app.put('/:userId/meetings/:meetingId', async (req, res) => {
   const userId = req.params.userId;
@@ -283,7 +352,6 @@ app.put('/:userId/meetings/:meetingId', async (req, res) => {
   console.log(agenda, date_time)
 
   try {
-    // For simplicity, let's assume the user can only update their own meetings
     const query = 'UPDATE meetings SET Agenda = ?, Date_Time = ? WHERE Meeting_ID = ? AND Club_ID IN (SELECT ClubID FROM Club_User WHERE User_ID = ?)';
     await queryAsync(query, [agenda, date_time, meetingId, userId]);
     res.json({ message: 'Meeting updated successfully' });
@@ -294,12 +362,20 @@ app.put('/:userId/meetings/:meetingId', async (req, res) => {
 });
 
 
+
+/////////////////////////////////////////////////////////
+
+// Deleting meetings
+
+
+/////////////////////////////////////////////////////////
+
+
 app.delete('/:userId/meetings/:meetingId', async (req, res) => {
   const userId = req.params.userId;
   const meetingId = req.params.meetingId;
 
   try {
-    // For simplicity, let's assume the user can only delete their own meetings
     const query = 'DELETE FROM meetings WHERE Meeting_ID = ? AND Club_ID IN (SELECT ClubID FROM Club_User WHERE User_ID = ?)';
     await queryAsync(query, [meetingId, userId]);
     res.json({ message: 'Meeting deleted successfully' });
@@ -308,6 +384,14 @@ app.delete('/:userId/meetings/:meetingId', async (req, res) => {
     res.status(500).json({ message: 'Error deleting meeting' });
   }
 });
+
+/////////////////////////////////////////////////////////
+
+
+// Retrieving clubs the user is part of
+
+
+/////////////////////////////////////////////////////////
 
 app.get('/:userId/user_clubs', (req, res) => {
   const userId = req.params.userId;
@@ -320,12 +404,14 @@ app.get('/:userId/user_clubs', (req, res) => {
     if (err) {
       return res.status(500).json({ message: 'Error fetching user clubs', error: err });
     }
-    return res.json(data[0]); // Assuming the result is in the first element of the data array
+    return res.json(data[0]); 
   });
 });
 
 
-// ... (other routes remain unchanged)
+/////////////////////////////////////////////////////////
+
+
 
 
 
